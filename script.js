6a254d512336734d238b7094efd3db7d83e957f5
Shajidaa <shajidaakterlopa101@gmail.com>
2025-08-22 12:09:16 +0600

    update

--- script.js
+++ script.js
@@ -6,28 +6,33 @@ let btn=document.getElementById('submit-btn');
 let height=document.getElementById('input-height');
 
 let noteOfTheBmi=document.getElementById('note');
-
+let inputSection=document.getElementById('input-section');
+    let outputSection=document.getElementById('output-section');
 // function
 
 btn.addEventListener('click',function () {
      height=document.getElementById('input-height').value;
 
     let weight=document.getElementById('input-weight').value;
-    let inputSection=document.getElementById('input-section')
-    let outputSection=document.getElementById('output-section')
+    
  
     const finalBMI=document.getElementById('result');
 
+    // if ( typeof height=== "string" ||typeof height=== "null"|| 
+    //     typeof weight=== "string"||typeof weight=== "null" ) {
+         
+
+    // }
     
     
-    const calculationValue=Number(weight)/(Number(height)*Number(height));
+    const calculationValue=Number(weight)/(Number(height*.3048)*Number(height*0.3048));
     
     finalBMI.value=calculationValue.toFixed(2);
 
     //BMi chart 
 
  if (finalBMI.value<16) {
-      noteOfTheBmi.innerText='Severe Thinness'
+      noteOfTheBmi.innerText='Severe Thinness '
    }else if(finalBMI.value<17 ){
         noteOfTheBmi.innerText='Moderate Thinness'
    }else if(finalBMI.value<25){
@@ -47,4 +52,13 @@ btn.addEventListener('click',function () {
 }
 )
 
+const resetBtn=document.querySelector('#reset');
 
+resetBtn.addEventListener('click',function () {
+    window.location.reload(true); 
+    inputSection.style.display='flex';
+
+    
+   outputSection.style.display='none'
+
+})
